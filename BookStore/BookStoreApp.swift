831eed7e8e68c5fa6465aac3d52b0cb5708e6a64
Asmaa Eliwa <asmaagamalelsayed96@gmail.com>
2024-03-28 11:11:46 -0400

    setting up app

---
+++ BookStore/BookStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookStoreApp.swift
+//  BookStore
+//
+//  Created by asmaa gamal  on 28/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct BookStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
